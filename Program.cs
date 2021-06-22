b52f57d19c6a25204d7d1063d69f4fde7dac6ce2
Zina Bezvershenko <zina.bezvershenko@ge.com>
2021-06-21 23:15:26 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Prime.Services;
+
+namespace check4prime
+{
+    class Program
+    {
+	private readonly PrimeService _primeService;
+
+        static void Main(string[] args)
+        {
+	    _primeService = new PrimeService(); 	
+            Console.WriteLine(string.Format("Hello World! {0}", _primeService.IsPrime(2)));
+        }
+    }
+}
